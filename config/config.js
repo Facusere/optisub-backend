ddc03f3777053977d7b70b6d28b775f65ff417d5
Facundo <facusere@gmail.com>
2025-06-11 19:44:59 -0300

    Cambio de base de datos de Railway a Neon

--- config/config.js
+++ config/config.js
@@ -2,26 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'optisub_dev',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
-    username: 'postgres',
-    password: 'admin',
-    database: 'optisub_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -31,5 +32,3 @@ module.exports = {
     }
   }
 };
-
-
